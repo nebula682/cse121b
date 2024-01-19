9b0894a760adf2be8044c65af349da3866939bff
nebula682 <malcolmnie@gmail.com>
2024-01-19 14:46:00 +0200

    week 2 tasks

---
+++ scripts/w02-task.js
@@ -0,0 +1,56 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName =" MALCOLM NIGEL NKOMO";
+const currentYear = new Date().getFullYear();
+const profilePicture = "images/Mal.jpg";
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML =`<strong> ${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute ("src",profilePicture);
+imageElement.setAttribute ("alt",`This is ${fullName}, an online student at BYU Idaho`);
+
+
+
+
+
+
+/* Step 5 - Array */
+let favFoods = ["Pizza", "Ice Cream", "Cake", "Milkshake", " Shawarma", "Sadza", "Ribs"," Rice and Chicken","Steak", "Burgers"];
+document.getElementById("food").innerHTML = favFoods;
+let newFood ="Mangoes"
+favFoods.push(newFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
